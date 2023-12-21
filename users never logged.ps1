6371141f7e4dd76228e4330340f3cf4d804f9f82
Tenaka <86342641+Tenaka@users.noreply.github.com>
2023-12-21 06:37:39 +0000

    Add files via upload

---
+++ users never logged.ps1
@@ -0,0 +1,18 @@
+﻿Get-ADUser -ldapfilter '(&(!lastlogontimestamp=*)(!useraccountcontrol:1.2.840.113556.1.4.803:=2))' |
+ Select Name,DistinguishedName
+
+ Get-ADUser -Filter {(lastlogontimestamp -notlike "*") -and (enabled -eq $true)} | Select Name,DistinguishedName
+
+
+$createdtime = (Get-Date).Adddays(-(30))
+Get-ADUser -Filter {(lastlogontimestamp -notlike "*") -and (enabled -eq $true) -and (whencreated -lt $createdtime)} | 
+Select Name,DistinguishedName |
+Export-CSV "C:\NeverLoggedOnUsers.csv" -NoTypeInformation -Encoding UTF8
+
+
+
+Get-ADUser -Filter { Enabled -eq $True } -Properties LastLogonDate |
+Where-Object { $_.LastLogonDate -lt (Get-Date).AddDays(-60) -or -not $_.LastLogonDate } |
+Select-Object -Property SamAccountName 
+ 
+
